123591b1563edecc9a77e115f477f118a118338d
Darryl Wagoner <darryl@wagoner.me>
2024-10-05 17:22:45 -0500

    PubSub working

---
+++ NatByExample_net8/Publist_Subscribe/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Publist_Subscribe;
+
+var i = 0;
+
+var simplePubSub = new PubSubscribe();
+await simplePubSub.PubSub();
+Console.WriteLine("*****************************************************************************");
+await simplePubSub.ReqReply();
